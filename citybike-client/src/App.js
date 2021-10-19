aa437a36b0cc63be717e0ad6517b449839fda930
Jet Lawrence Doctor <jetlawrence@users.noreply.github.com>
2021-10-19 22:38:34 +0800

    Upgrade dependencies, change to hooks

--- citybike-client/src/App.js
+++ citybike-client/src/App.js
@@ -1,40 +1,35 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import socketIOClient from "socket.io-client";
-import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [state, setState] = useState({
+    response: false,
+    endpoint: "http://127.0.0.1:4001",
+    lat: 51.505,
+    lng: -0.09,
+    zoom: 13,
+  });
 
-    this.state = {
-      response: false,
-      endpoint: "http://127.0.0.1:4001",
-      lat: 51.505,
-      lng: -0.09,
-      zoom: 13
-    };
-
-  }
-  componentDidMount() {
-    const { endpoint } = this.state;
+  useEffect(() => {
+    const { endpoint } = state;
     const socket = socketIOClient(endpoint);
-   
-  }
-  render() {
-    const { response } = this.state;
-    const position = [this.state.lat, this.state.lng]
-    return (
+  }, []);
 
-      <div className="map">
-        <h1> City Bikes in Miami </h1>
-        <Map center={position} zoom={this.state.zoom}>
-          <TileLayer
-            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-        </Map>
-      </div>
-    );
-  }
-}
+  const response = state?.response;
+  const position = [state?.lat, state?.lng];
+
+  return (
+    <div className="map">
+      <h1> City Bikes in Miami </h1>
+      <MapContainer center={position} zoom={state?.zoom}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
+};
+
 export default App;
